bbd61c5d1d8d1a64a7dace2bdfaf86dcdcda55e8
15860159070 <15860159070@cloudtogo.cn>
2020-02-10 13:38:49 +0000

    Cloud To Go is Amazing!

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# 请您来修正Hello, word的拼写错误吧！11111
+# 请您来修正Hello, word的拼写错误吧！11111ggggg
 # 请在修改完成后，通过主菜单 Git/Commit ... 菜单项完成代码的Commit 和 Push。
 # Push完成后回到Factory ( http://factory.cloudtogo.cn/project/blueprint?id=last )，用同样的方法发布一个新实例即可看到修改后的效果。
 
